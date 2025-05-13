d250aef9c170f5c1758dc44cbbd09262ac370518
Shreya Nandaghaye <147903310+shreyanandaghaye946@users.noreply.github.com>
2025-05-14 00:19:33 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-6k_+vs6l557#88qlq*h9)4)*@kmo6l%6mtq!)f&e@#wt**t5eh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-6k_+vs6l557#88qlq*h9)4)*@kmo6l%6mtq!)f&e@#wt**t5eh')
 
-DEBUG = True
+DEBUG = os.environ.get('RENDER', '') != 'TRUE'  # DEBUG is False on Render
 
-ALLOWED_HOSTS = []  # Add your domain/IP here in production
+ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # for static file serving
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -34,7 +35,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'ecommerceapp' / 'templates'],  # FIXED position
+        'DIRS': [BASE_DIR / 'ecommerceapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -57,18 +58,10 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -76,23 +69,18 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
 USE_TZ = True
-
-# LOGIN_URL = '/login_view/'
 
 LOGIN_URL = '/login/'
 
-
+# Static files setup for Render
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'ecommerceapp' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # required for collectstatic
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'ecommerceapp' / 'static',  # optional but useful
-]
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
